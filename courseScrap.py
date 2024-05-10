8cfda910b631d225fdf68fd0b850d2b1de27ac00
admin_li2177 <admin_li2177@ads.mcmaster.ca>
2024-05-10 15:14:52 -0400

    up to begining of searchCourse

--- courseScrap.py
+++ courseScrap.py
@@ -1,10 +1,13 @@
 from urllib.request import urlopen
 import re
 from bs4 import BeautifulSoup
+import json
+import mechanicalsoup
+from selenium import webdriver
 
 
-def searchCourse():     #
-    "todo"
+def searchCourse(keyWord: str):     #
+    browser = webdriver.Chrome()
 
 def fetchCourse(url: str):  #find (title, content, #block) from url at KU course website
     courseInstance = urlopen(url)
@@ -13,21 +16,32 @@ def fetchCourse(url: str):  #find (title, content, #block) from url at KU course
     #title
     soup = BeautifulSoup(courseCode, "html.parser")
     title = soup.find("h1").string
-    print(title)
 
     #content
     contentSoup = soup.find("div", id="course-content")
     contents = contentSoup.get_text()
-    print(contents)
 
     #block number
     pattern = "<dd>Block .</dd>"
     searchResult = re.search(pattern, courseCode)
     result = searchResult.group()
     result = re.sub("<dd>Block ", "", result)
-    result = re.sub("</dd>", "", result)
-    print(result)
+    block = re.sub("</dd>", "", result)
+    courseInfo = {}
+    courseInfo['title'] = title
+    courseInfo['content'] = contents
+    courseInfo['block'] = block
+    return courseInfo
 
-fetchCourse("https://kurser.ku.dk/course/ndak22000u/2023-2024")
+""" testUrl = "https://kurser.ku.dk/course/ndak22000u/2023-2024"
+
+testDict = fetchCourse(testUrl)
+
+testUrl2 = "https://kurser.ku.dk/course/ndaa09023u/2024-2025"
+testDict2 = fetchCourse(testUrl2)
+
+with open('courseData.json', 'w') as courseDataFile:
+    json.dump([testDict, testDict2], courseDataFile, indent=1) """
+
 
  
